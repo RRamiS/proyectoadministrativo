ad99ce69acb022d0d59ad86baf8b7ca541589285
Ramiro <ramirolopez04902@gmail.com>
2024-12-01 14:26:30 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,16 +8,16 @@ import { Auth0Provider } from '@auth0/auth0-react'; // Importar Auth0Provider
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="https://dev-vzdnsztoc6gy1f35.us.auth0.com" // Reemplaza con tu dominio de Auth0
-      clientId="YmlcuWRn6boyQZuxjGMEXxMtdEIIDh0V" // Reemplaza con tu Client ID
-      authorizationParams={{
-        redirect_uri: window.location.origin, // Redirige a la raíz después de login
-        audience: "https://dev-vzdnsztoc6gy1f35.us.auth0.com/api/v2/", // Reemplaza con tu audiencia si usas una API
-      }}
-    >
-      <App />
-    </Auth0Provider>
+<Auth0Provider
+  domain="dev-vzdnsztoc6gy1f35.us.auth0.com"
+  clientId="YmlcuWRn6boyQZuxjGMEXxMtdEIIDh0V"
+  authorizationParams={{
+    redirect_uri: window.location.origin + '/callback',
+    audience: "https://dev-vzdnsztoc6gy1f35.us.auth0.com/api/v2/",
+  }}
+>
+  <App />
+</Auth0Provider>
   </React.StrictMode>
 );
 
