27c6e21f614be06274e402e3d041ce39eeb93840
Ramiro <ramirolopez04902@gmail.com>
2024-12-01 17:18:56 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import AdminDashboard from './Administracion/componentes/AdminDashboard';
 import FolderTable from './Administracion/componentes/FolderTable';
-import LoginLogoutButton from './Administracion/componentes/LoginLogoutButton';
 
 const App = () => {
+  const { handleRedirectCallback, isLoading } = useAuth0();
+
+  useEffect(() => {
+    const processLogin = async () => {
+      if (window.location.search.includes('code=') && window.location.search.includes('state=')) {
+        try {
+          await handleRedirectCallback();
+          // Limpia la URL para evitar confusiones
+          window.history.replaceState({}, document.title, window.location.pathname);
+        } catch (error) {
+          console.error('Error al procesar el login:', error);
+        }
+      }
+    };
+    processLogin();
+  }, [handleRedirectCallback]);
+
+  if (isLoading) {
+    return <div>Cargando...</div>; // Muestra un estado de carga mientras Auth0 verifica el estado
+  }
+
   return (
     <Router>
       <div>
-        <LoginLogoutButton />
         <Routes>
           <Route path="/" element={<AdminDashboard />} />
           <Route path="/folders" element={<FolderTable />} />
