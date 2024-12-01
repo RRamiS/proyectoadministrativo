0e12aee5849c028db4a156b90449d441e97491f3
Ramiro <ramirolopez04902@gmail.com>
2024-12-01 15:58:36 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Auth0Provider, useAuth0 } from '@auth0/auth0-react'; // Importa useAuth0
-
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 import AdminDashboard from '../src/Administracion/componentes/AdminDashboard';
 import FolderTable from '../src/Administracion/componentes/FolderTable';
 
-// Reemplaza con tus valores de Auth0
-const AUTH0_DOMAIN = "https://dev-vzdnsztoc6gy1f35.us.auth0.com";
+// Valores de configuración
+const AUTH0_DOMAIN = "dev-vzdnsztoc6gy1f35.us.auth0.com";
 const AUTH0_CLIENT_ID = "YmlcuWRn6boyQZuxjGMEXxMtdEIIDh0V";
 const AUTH0_AUDIENCE = "https://dev-vzdnsztoc6gy1f35.us.auth0.com/api/v2/";
-
-// Componente Callback para manejar la autenticación
-const Callback = () => {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
-
-  React.useEffect(() => {
-    if (!isAuthenticated) {
-      loginWithRedirect();
-    }
-  }, [isAuthenticated, loginWithRedirect]);
-
-  return isAuthenticated ? (
-    <div>Redirigiendo...</div>
-  ) : (
-    <div>Error al autenticar</div>
-  );
-};
 
 function App() {
   return (
@@ -42,7 +24,6 @@ function App() {
         <Routes>
           <Route path="/" element={<AdminDashboard />} />
           <Route path="/carpetas/:id" element={<FolderTable />} />
-          <Route path="/callback" element={<Callback />} />
         </Routes>
       </Router>
     </Auth0Provider>
--- src/index.js
+++ src/index.js
@@ -3,21 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react'; // Importar Auth0Provider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<Auth0Provider
-  domain="https://dev-vzdnsztoc6gy1f35.us.auth0.com"
-  clientId="YmlcuWRn6boyQZuxjGMEXxMtdEIIDh0V"
-  authorizationParams={{
-    redirect_uri: "https://proyectoadministrativo.vercel.app/callback",
-    audience: "https://dev-vzdnsztoc6gy1f35.us.auth0.com/api/v2/",
-  }}
->
-  <App />
-</Auth0Provider>
+    <App />
   </React.StrictMode>
 );
 
